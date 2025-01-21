5e71f8fd19b6ab35b39f5216d734001436d03811
mayildiz1 <33671986+mayildiz1@users.noreply.github.com>
2025-01-21 11:00:56 -0600

    Added dark mode based on time of the day

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const dateWord = document.getElementById("date-coding");
+const documentBody = document.querySelector("html");
+const navMenu = document.querySelectorAll("a");
+const footer = document.querySelector("footer");
+
 const myDate = new Date("1998-09-01T18:00:00");
 const today = new Date();
+const nowHour = new Date().getHours();
 
 function findPeriod() {
   let dateDiff = today - myDate;
@@ -15,6 +20,15 @@ function findPeriod() {
   const seconds = Math.floor(dateDiff / 1000);
 
   return `${years} years ${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds`;
+}
+
+if (nowHour >= 19 && nowHour < 6) {
+  documentBody.style.backgroundColor = "black";
+  documentBody.style.color = "white";
+  navMenu.forEach((element) => {
+    element.style.color = "white";
+  });
+  footer.style.borderTop = "1px solid white";
 }
 
 dateWord.innerHTML = findPeriod();
